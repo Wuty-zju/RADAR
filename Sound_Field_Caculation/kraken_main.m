bb0efbfe8cccb847b52311f2e1e64dae7ef3b773
吴天宇 <154712857+Wuty-zju@users.noreply.github.com>
2024-01-05 14:03:39 +0000

    2

--- Sound_Field_Caculation/kraken_main.m
+++ Sound_Field_Caculation/kraken_main.m
@@ -18,14 +18,14 @@ system('.\lib\field.exe .\env\kraken_Pekeris_range_depth')
  
 %% 传播损失
 figure(1)
- plotshd('.\env\kraken_Ideal_range.shd')
+plotshd('.\env\kraken_Ideal_range.shd')
 figure(2)
- plotshd('.\env\kraken_Ideal_range_depth.shd')
+plotshd('.\env\kraken_Ideal_range_depth.shd')
 
 figure(3)
- plotshd('.\env\kraken_Pekeris_range.shd')
+plotshd('.\env\kraken_Pekeris_range.shd')
 figure(4)
- plotshd('.\env\kraken_Pekeris_range_depth.shd')
+plotshd('.\env\kraken_Pekeris_range_depth.shd')
  
 %% 模式
 plotmode('.\env\kraken_Ideal_range_depth', 500, [ 1 2 3 4 ])
