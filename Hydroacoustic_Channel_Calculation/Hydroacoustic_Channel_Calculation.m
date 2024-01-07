627c69503ce1cbe5dd50bc58fc98f0cb49b93fc4
吴天宇 <154712857+Wuty-zju@users.noreply.github.com>
2024-01-07 13:09:33 +0000

    1

---
+++ Hydroacoustic_Channel_Calculation/Hydroacoustic_Channel_Calculation.m
@@ -0,0 +1,7 @@
+% 整合Hydroacoustic_Channel_Calculation/lib中的.m文件统一运行
+
+% Add the path to the lib directory
+addpath('./Hydroacoustic_Channel_Calculation/lib');
+
+run('./lib/Bellhop_data.m');
+run('./lib/PSK4.m');
